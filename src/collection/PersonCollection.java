d2eed6fd0ce249063b9dec18e43b30691ace3813
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-03-05 16:32:13 +0300

    Add PersonCollection.java

---
+++ src/collection/PersonCollection.java
@@ -0,0 +1,18 @@
+package collection;
+
+import client.ClientManager;
+import data.Person;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+import static java.time.LocalDate.*;
+
+/**
+ * A class that implements collection related methods
+ */
+public class PersonCollection {
+    private static TreeSet<Person> treeSet;
+    private static LocalDate creationDate;
+}
