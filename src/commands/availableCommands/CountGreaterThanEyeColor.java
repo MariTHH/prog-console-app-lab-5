2ecd7ac6c80ac425ed0123d07b03329cf225bbc7
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-03-05 16:43:49 +0300

    Add command CountGreaterThanEyeColor.java

---
+++ src/commands/availableCommands/CountGreaterThanEyeColor.java
@@ -0,0 +1,36 @@
+package commands.availableCommands;
+
+import client.ReadManager;
+import collection.PersonCollection;
+import commands.Command;
+import data.Color;
+
+/**
+ * count_greater_than_eye_color eyeColor :
+ * display the number of elements whose eyeColor field value is greater than the set value
+ */
+public class CountGreaterThanEyeColor extends Command {
+
+    @Override
+    public void execute(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Неправильный формат команды! Команда не будет выполнена.");
+        } else {
+            countGreater(args[1]);
+        }
+    }
+
+    public static void countGreater(String eyeColor_s) {
+        int code;
+        try {
+            if (ReadManager.doesThisColorTypeExist(eyeColor_s)) {
+                code = Enum.valueOf(Color.class, eyeColor_s).getCode();
+                PersonCollection.countEyeColor(code);
+            } else {
+                System.out.println("Такого цвета нет в списке");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Вы неправильно ввели команду");
+        }
+    }
+}
