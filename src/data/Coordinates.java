cad689480c66a1619117d0f13450cf1cb79166a2
Maria Tolstykh <118300117+MariTHH@users.noreply.github.com>
2023-03-05 16:31:44 +0300

    add Coordinates.java

---
+++ src/data/Coordinates.java
@@ -0,0 +1,19 @@
+package data;
+
+public class Coordinates {
+    private final long x;
+    private final int y;
+
+    public Coordinates(long x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
