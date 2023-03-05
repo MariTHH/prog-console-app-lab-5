0a97ef30f71fc723b2dbee2679886d1c0d515f51
Maria Tolstykh <118300117+MariTHH@users.noreply.github.com>
2023-03-05 18:58:02 +0300

    add CommandManager for commands

---
+++ src/commands/CommandManager.java
@@ -0,0 +1,52 @@
+package commands;
+
+import collection.PersonCollection;
+import commands.availableCommands.*;
+
+import java.util.HashMap;
+
+public class CommandManager {
+
+    private static PersonCollection personCollection;
+    private final static HashMap<String, Command> commandMap;
+
+    static {
+        commandMap = new HashMap<>();
+        commandMap.put("add", new Add());
+        commandMap.put("add_if_max", new AddIfMax());
+        commandMap.put("add_if_min", new AddIfMin());
+        commandMap.put("clear", new Clear());
+        commandMap.put("remove_by_id", new RemoveById());
+        commandMap.put("show", new Show());
+        commandMap.put("removeGreater", new RemoveGreater());
+        commandMap.put("countGreater", new CountGreaterThanEyeColor());
+        commandMap.put("update", new Update());
+    }
+
+    public static HashMap<String, Command> getCommandMap() {
+        return commandMap;
+    }
+
+    public static void setPersonCollection(PersonCollection personCollection) {
+        CommandManager.personCollection = personCollection;
+    }
+
+    public static PersonCollection getPersonCollection() {
+        return CommandManager.personCollection;
+    }
+
+    public void existCommand(String[] name) {
+        try {
+            if (name.length > 0) {
+                Command command = commandMap.get(name[0]);
+                command.execute(name);
+            } else {
+                System.out.println("Вы не ввели команду");
+            }
+        } catch (IllegalStateException | NullPointerException e) {
+            if (!name[0].equals("")) {
+                System.out.println("Команды " + name[0] + " не существует");
+            }
+        }
+    }
+}
