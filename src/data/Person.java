8862b380cf478fad9233c9904d397c66830cffe7
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-03-05 16:16:11 +0300

    Add set and get for Person

--- src/data/Person.java
+++ src/data/Person.java
@@ -28,5 +28,84 @@ public class Person {
         this.location = location;
     }
 
+    public String getName() {
+        return this.name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public Coordinates getCoordinates() {
+        return this.coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public LocalDate getCreationDate() {
+        return this.creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color getEyeColor() {
+        return this.eyeColor;
+    }
+
+    public void setEyeColor(Color eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public Color getHairColor() {
+        return this.hairColor;
+    }
+
+    public void setHairColor(Color hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public Country getNationality() {
+        return this.nationality;
+    }
+
+    public void setNationality(Country nationality) {
+        this.nationality = nationality;
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public int compareTo(Person person) { // height comparison
+        if (this.height - person.getHeight() > 0) {
+            return 1;
+        } else {
+            if (this.height - person.getHeight() == 0) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+    }
+    
 }
