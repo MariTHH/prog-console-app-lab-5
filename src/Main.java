5756441dbc8fc671897401e8869ecc54fd74feee
Maria Tolstykh <118300117+MariTHH@users.noreply.github.com>
2023-03-07 18:23:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
-import data.*;
+import collection.PersonCollection;
+import commands.CommandManager;
 
-
-import java.util.Comparator;
+import java.util.Scanner;
 import java.util.TreeSet;
 
 import comparator.HeightComparator;
 
 public class Main {
     public static void main(String[] args) {
-        Comparator<Person> personComparator = new HeightComparator(); // sorting check
-        TreeSet<Person> persons = new TreeSet(personComparator);
-        persons.add(new Person("Tom", new Coordinates(2, 3), 165, Color.BLACK, Color.BLACK, Country.CHINA, new Location(2.0, 4, "place1")));
-        persons.add(new Person("Tom1", new Coordinates(88, 88), 8000, Color.BLACK, Color.BLACK, Country.CHINA, new Location(4.0, 5, "place")));
 
-        for (Person p : persons) {
-            System.out.println(p.getHeight() + " " + p.getName());
-        }
+        PersonCollection collection = new PersonCollection();
+        CommandManager.setPersonCollection(collection);
+        PersonCollection.initializeCollection();
+        Scanner sc = new Scanner(System.in);
+        CommandManager commandManager = new CommandManager();
+        CommandManager.existCommand(new String[]{"add"});
+        CommandManager.existCommand(new String[]{"help"});
     }
 }
