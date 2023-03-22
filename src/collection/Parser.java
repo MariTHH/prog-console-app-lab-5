263a0cf8c7c74842922e9eaa0aef272984b3a4d9
Maria Tolstykh <118300117+MariTHH@users.noreply.github.com>
2023-03-22 21:26:07 +0300

    add reverse parser and exception

--- src/collection/Parser.java
+++ src/collection/Parser.java
@@ -12,12 +12,22 @@ public final class Parser {
     }
 
     public static void convertToXML(PersonCollection collection, String filename) throws JAXBException, IOException {
+        try{
+            JAXBContext context = JAXBContext.newInstance(PersonCollection.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(filename));
+            marshaller.marshal(collection, bufferedOutputStream);
+            bufferedOutputStream.close();
+           } catch (IOException | JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static PersonCollection convertToJavaObject(File filename) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(PersonCollection.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(filename));
-        marshaller.marshal(collection, bufferedOutputStream);
-        bufferedOutputStream.close();
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        return (PersonCollection) unmarshaller.unmarshal(filename);
     }
 }
