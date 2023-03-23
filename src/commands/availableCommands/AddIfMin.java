5603bcaabca904c819294bfa13467bf749d8ca94
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-03-23 12:00:49 +0300

    Fix AddIfMin

--- src/commands/availableCommands/AddIfMin.java
+++ src/commands/availableCommands/AddIfMin.java
@@ -10,16 +10,19 @@ import java.util.Scanner;
  * add a new element to the collection if its value is less than the smallest element of that collection
  */
 public class AddIfMin extends Command {
-    private PersonCollection personCollection;
+    private final PersonCollection personCollection;
+
+    public AddIfMin (PersonCollection personCollection) {
+        this.personCollection = personCollection;
+    }
 
     @Override
     public void execute(String[] args) {
-        if (args.length != 0) {
+        if (args.length > 1) {
             System.out.println("Вы неправильно ввели команду");
         } else {
             Scanner sc = new Scanner(System.in);
             personCollection.addIfMin(sc);
-            System.out.println("Персонаж добавлен");
         }
     }
 }
