bd94ae18d680fc8bba7788cc7736aec663760a56
Maria Tolstykh <118300117+MariTHH@users.noreply.github.com>
2023-03-23 12:10:11 +0300

    fix Command

--- src/commands/Command.java
+++ src/commands/Command.java
@@ -1,6 +1,14 @@
 package commands;
 
 public abstract class Command {
+    private Object argument;
 
-    public abstract void execute(String[] args);
+    public abstract void execute(String[] args) throws JAXBException, IOException, FileNotFoundException;
+    public Object getArgument() {
+        return argument;
+    }
+
+    public void setArgument(String argument) {
+        this.argument = argument;
+    }
 }
