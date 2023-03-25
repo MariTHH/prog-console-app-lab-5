e7c4f22e251d7a601f576ae941947a84fc0c202c
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-03-25 18:52:06 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,29 @@ import collection.PersonCollection;
 import commands.CommandManager;
 
 import java.util.Scanner;
+import java.io.IOException;
 import java.util.TreeSet;
 
-import comparator.HeightComparator;
+import javax.xml.bind.JAXBException;
+
+import static collection.Parser.convertToJavaObject;
 
 public class Main {
-    public static void main(String[] args) throws JAXBException, IOException {
-        PersonCollection collection = new PersonCollection();
-        new CommandManager(collection);
-        while (CommandManager.getWork()) {
-            CommandManager.existCommand();
+    public static void main(String[] args) throws IOException, IllegalArgumentException {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            String Path = scanner.nextLine();
+            File file = new File(String.valueOf(Path));
+            PersonCollection collection = new PersonCollection();
+            collection.setCollection(convertToJavaObject(file).getCollection());
+            new CommandManager(collection);
+            while (CommandManager.getWork()) {
+                CommandManager.existCommand();
+            }
+        } catch (JAXBException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Приложение не может запуститься");
+
         }
     }
 }
